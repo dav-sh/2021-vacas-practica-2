77be03d47da37001ad03c9a76758c7406b151153
David <dav.llwyr@gmail.com>
2021-06-16 19:23:45 -0600

    agregue la clase peliculas

---
+++ src/peliculas/Peliculas.java
@@ -0,0 +1,45 @@
+public class Peliculas {
+    int id,anio;
+    String nombre,categoria;
+    boolean disponible;
+    public Peliculas(int id, int anio, String nombre, String categoria,boolean disponible) {
+        this.id = id;
+        this.anio = anio;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setDisponible(String disponible) {
+        this.disponible = disponible;
+    }
+    public String getDisponible() {
+        return disponible;
+    }
+    public String getDatos(){
+        return "id: "+id+" Nombre: "+nombre+ " Categoria: "+categoria+" Disponible: "+disponible;
+    }
+}
